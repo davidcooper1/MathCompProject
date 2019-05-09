973e50407670b9ee838c100e9e2563e311a9cb37
davidcooper1 <davidpcooper1@gmail.com>
2019-05-08 21:36:48 -0500

    Simplified functions and added docstrings.

--- computation.py
+++ computation.py
@@ -1,176 +1,324 @@
-def _conjugate(num) :
-    return complex(num.real, -num.imag)
+def _abs(n) :
+    """Computes the absolute value of a number.
 
-class Vector :
+    Given any number real or complex, this function returns its
+    absolute value.
+
+    Args:
+        n (int/float/complex): The number to find the absolute value of.
+
+    Returns:
+        float: The absolute value of n.
+
     """
-        This class is meant to represent a mathematical vector.
-        It will overload standard operators in order to make use of these vectors more natural.
+    return ((n.real ** 2) + (n.imag ** 2)) ** (1/2)
+
+def _sum(iter) :
+    """Computes the sum of all elements in an iterable.
+
+    This function will loop through all elements of the iterable,
+    iter, and compute their sum. It assumes that all of the elements
+    of iter are of a numerical type.
+
+    Args:
+        iter (iterable): The iterable containing all the numbers to sum.
+
+    Returns:
+        (int/float/complex): The sum of the elements in the iterable.
+
     """
 
-    VERTICAL = 0
-    HORIZONTAL = 1
+    result = 0
+    for x in iter :
+        result += x
+    return result
 
-    def __init__(self, val=[0], orientation=VERTICAL) :
-        self.val = val
-        self.orientation = orientation
+def _conjugate(n) :
+    """Computes the complex conjugate of a number.
 
-    def __str__(self) :
-        return str(self.val)
+    This function will compute the complex conjugate of a number by
+    constructing a new complex number from the real part of the numbers
+    with the negatiion of the imaginary part of the number.
 
-    def __len__(self) :
-        return len(self.val)
+    Args:
+        n (int/float/complex): The number to find the complex conjugate of.
 
-    def __getitem__(self, index) :
-        return self.val[index]
+    Returns:
+        (int/float): If the number is not a complex number, do nothing.
+        (complex): If the number is complex, return its conjugate.
 
-    def __setitem__(self, index, value) :
-        self.val[index] = value
-        return self.val[index]
+    """
 
-    def __neg__(self) :
-        return Vector([-self[i] for i in range(len(self))], self.orientation)
+    if (type(n) is int or type(n) is float) :
+        return n;
+    return complex(n.real, -n.imag)
 
-    def __pos__(self) :
-        return Vector(self.val, self.orientation)
+def scalarVecMulti(n, v) :
+    """Computes the result of scalar vector multiplication.
 
-    def __add__(self, other) :
-        if type(other) is Vector :
-            if len(self) == len(other) :
-                return Vector([self[i] + other[i] for i in range(len(self))], self.orientation)
-            else :
-                raise TypeError("Vectors are of incompatible dimensions: {} and {}".format(len(self), len(other)))
-        else :
-            return NotImplemented
+    This function takes a vector and multiplies all of its elements
+    by a number to create a new vector.
 
-    def __sub__(self, other) :
-        return self.__add__(-other)
+    Args:
+        n (int/float/complex): The number to multiply by all vector elements.
+        v (list): The list of numbers representing the vector to be multiplied.
 
-    def dot(self, other) :
-        if type(other) is Vector:
-            if len(self) == len(other) :
-                mult = [self[i] * other[i] for i in range(len(self))]
-                result = 0
-                for i in range(len(mult)) :
-                    result += mult[i]
-                return result
-            else :
-                raise TypeError("Vectors are of incompatible dimensions: {} and {}".format(len(self), len(other)))
-        else :
-            raise TypeError("Expected Vector but found type {}".format(type(other)))
+    Returns:
+        (list): The result of multiplying every element of v by n.
 
-    def __mul__(self, other) :
-        if type(other) is int or type(other) is float or type(other) is complex :
-            return Vector([self[i] * other for i in range(len(self))], self.orientation)
-        elif type(other) is Matrix :
-            result = Matrix(self) * other
-            if result.rows() == 1 :
-                return result.row(0)
-            elif result.cols() == 1 :
-                return result.col(0)
-            else :
-                return result
-        else :
-            return NotImplemented
+    """
+    return [n * v[i] for i in range(len(v))]
 
-    def __rmul__(self, other) :
-        if type(other) is int or type(other) is float or type(other) is complex :
-            return Vector([self[i] * other for i in range(len(self))], self.orientation)
-        elif type(other) is Matrix :
-            result = other * Matrix(self)
-            if result.rows() == 1 :
-                return result.row(0)
-            elif result.cols() == 1 :
-                return result.col(0)
-            else :
-                return result
-        else :
-            return NotImplemented
+def conjTransMat(M) :
+    """ Computes the complex conjugate of a matrix.
 
-    def transpose(self) :
-        return Vector(self.val, 1 ^ self.orientation)
+    This function flips the indices of the matrix M and computes the
+    complex conjugate of all the entries in the matrix.
 
-class Matrix :
+    Args:
+        M (list): This is a list of lists representing a matrix to find the conjugate transpose of.
 
-    @staticmethod
-    def zeros(rows, cols) :
-        return Matrix([[0 for col in range(cols)] for row in range(rows)])
+    Returns:
+        (list): The list of lists representing a matrix that is the conjugate transpose of M.
 
-    @staticmethod
-    def ones(rows, cols) :
-        return Matrix([[1 for col in range(cols) for row in range(rows)]])
+    """
 
-    def __init__(self, val=[[0]]) :
-        if type(val) is Vector :
-            if val.orientation == Vector.VERTICAL :
-                self.val = [[val[i]] for i in range(len(val))]
-                return
-            elif val.orientation == Vector.HORIZONTAL :
-                self.val = [[val[i] for i in range(len(val))]]
-                return
-        self.val = val
+    return [
+        [_conjugate(M[j][i]) for j in range(len(M)) ] for i in range(len(M[0]))
+    ]
 
-    def copy() :
-        return Matrix([
-            [ self.val[i][j] for j in self.cols() ] for i in self.rows()
-        ])
+def conjTransVec(v) :
+    """Computes the conjugate transpose of a vector.
 
-    def __str__(self) :
-        return str(self.val)
+    This function takes the conjugate of every number in the
+    list representing a vector.
 
-    def __getitem__(self, index) :
-        return self.val[index]
+    Args:
+        v (list): This is the vector from which the complex transpose is found.
 
-    def cols(self) :
-        if len(self.val) > 0 :
-            return len(self.val[0])
-        return 0
+    Returns:
+        (list): The complex transpose of the list v.
 
-    def rows(self) :
-        return len(self.val)
+    """
 
-    def col(self, index) :
-        if index >= self.cols() :
-            raise KeyError("Index is out of bounds.")
-        result = [0] * self.rows();
-        for i in range(self.rows()) :
-            result[i] = self.val[i][index]
-        return Vector(result, Vector.HORIZONTAL)
+    return [_conjugate(v[i]) for i in range(len(v))]
 
-    def row(self, index) :
-        if index >= self.rows() :
-            raise KeyError("Index is out of bounds.")
-        return Vector(list(self.val[index]), Vector.VERTICAL)
+def pNorm(v, p=2) :
+    """Computes the pNorm of a vector.
 
-    def __mul__(self, other) :
-        if type(other) is Matrix :
-            if self.cols() != other.rows() :
-                raise TypeError("Matrices are of incompatible dimensions: ({}, {}), ({}, {})".format(self.rows(), self.cols(), other.rows(), other.cols()))
-            return Matrix([
-                [
-                    self.row(row).dot(other.col(col))
-                    for col in range(other.cols())
-                ] for row in range(self.rows())
-            ])
-        elif type(other) is int or type(other) is float or type(other) is complex :
-            return Matrix([
-                [
-                    self.val[row][col] * other for col in range(self.cols())
-                ] for row in range(self.rows())
-            ])
-        else :
-            return NotImplemented
+    This function sums up all the elements of v raised to the p power
+    and then finds the pth root of the result of the sum.
 
-    def __rmul__(self, other) :
-        if type(other) is int or type(other) is float or type(other) is complex :
-            return self.__mul__(other)
-        return NotImplemented
+    Args:
+        v (list): A list representing a vector.
+        p (int): The power to be used for the norm operation. Defaults to 2.
 
-    def transpose(self) :
-        return Matrix([
-            self.col(i).val for i in range(self.cols())
-        ])
+    Returns:
+        (int/float): The p-norm of v. Always greater than zero.
 
-    def conjugate(self) :
-        return Matrix([
-            list(map(_conjugate, self.row(row).val)) for row in range(self.rows())
-        ])
+    """
+
+    return _sum([_abs(x) ** p for x in v]) ** (1 / p)
+
+def dotProduct(x, y) :
+    """Computes the dot product of two vectors.
+
+    This function sums the list formed by multiplying every element of one
+    vector with its corresponding element in another vector. It is assumed
+    that the lists are of equal length.
+
+    Args:
+        x (list): The list representing the first vector.
+        y (list): The list representing the second vector.
+
+    Returns:
+        (int/float/complex): The number representing the dot product of x and y.
+
+    """
+    return sum([x[i] * y[i] for i in range(len(x))])
+
+def vectorAdd(x, y) :
+    """Computes the result of the addition of two vectors.
+
+    This function returns a list in which every element from list x
+    has been added to its corresponding element in list y. It is assumed
+    the length of x and y match.
+
+    Args:
+        x (list): The list representing the first vector.
+        y (list): The list representing the second vector.
+
+    Returns:
+        (list): The result of vector addition between x and y.
+
+    """
+
+    return [x[i] + y[i] for i in range(len(x))]
+
+def vectorSub(x, y) :
+    """Computes the result of subtraction between two vectors.
+
+    This function returns a list in which every element of list y has been subtracted
+    from its corresponding element in list x such that the result is equal to
+    x - y. It is assumed the length of x and y match.
+
+    Args:
+        x (list): The list representing the first vector.
+        y (list): The list representing the second vector.
+
+    Returns:
+        (list): The result of vector subtraction between x and y.
+
+    """
+
+    return vectorAdd(x, scalarVecMulti(-1, y))
+
+def matRow(A, row) :
+    """Returns a copy of a row of the matrix.
+
+    This function takes a matrix of vertical vectors, A, and returns a copy
+    of the specified row. It is assumed that the variable row is a valid
+    row number (0 <= row <= len(A[0])).
+
+    Args:
+        A (list): This is a list of lists representing the matrix A.
+        row (int): This is the number of the row to be returned.
+
+    Returns:
+        (list): The specified row of the matrix.
+
+    """
+
+    return [A[i][row] for i in range(len(A))]
+
+def matrixMulti(A, B) :
+    """Computes the result of matrix multiplication.
+
+    This function takes two multidimensional lists representing
+    matrices and multiplies them by computing the dot product of every
+    row in A with every column in B. Forms the operation A * B.
+
+    Args:
+        A (list): Multidimensional list representing a matrix.
+        B (list): Multidimensional list representing a matrix.
+
+    Returns:
+        (list): Multidimensional list representing the result.
+
+    """
+
+    return [
+        [
+            dotProduct(
+                matRow(A, row),
+                B[col]
+            ) for row in range(len(A[0]))
+        ] for col in range(len(B))
+    ]
+
+def matVecMulti(A, b) :
+    """Computes the result of matrix vector multiplication.
+
+    This fuction takes in a multidimensional list representing a matrix,
+    A, and a list representing vector b. It then multiplies all the rows of
+    A with b itself. b is assumed to be a column vector. A has the same number
+    of columns as b has entries.
+
+    Args:
+        A (list): This is a multidimensional list representing a matrix.
+        b (list): This list represents a vector.
+
+    Returns:
+        (list): This is a list representing a vector result.
+
+    """
+
+    return [
+        dotProduct(matRow(A, row), b) for row in range(len(A[0]))
+    ]
+
+def vandermonde(x, n) :
+    """Constructs a vandermonde matrix.
+
+    This function takes in the list x, representing a vector, and
+    transforms it into a vandermonde matrix of degree n.
+
+    Args:
+        x (list): List representing a vector.
+        n (int): Number representing the degree.
+
+    Result:
+        (list): Multidimensional list representing the vandermonde matrix.
+
+    """
+
+    return [
+        [x[i] ** j for i in range(len(x))] for j in range(n)
+    ]
+
+def modGramSchmidt(A) :
+    """Computes the QR decomposition of a matrix
+
+    This function uses the modified gram schmidt algorithm to calculate
+    the QR decomposition of A. All matrices are represented as a set of
+    column vectors.
+
+    Args:
+        A (list): Multidimensional list representing a matrix.
+
+    Returns:
+        (list, list): Returns the matrices Q and R represented by multidimensional lists.
+
+    """
+
+    Q = []
+    R = [ [ 0 for i in range(len(A))] for j in range(len(A)) ]
+
+
+    v = [ [ A[i][j] for j in range(len(A[0])) ] for i in range(len(A)) ]
+
+    for j in range(len(v)) :
+        R[j][j] = pNorm(v[j])
+        Q.append(scalarVecMulti(1 / R[j][j], v[j]))
+
+        for k in range(j+1, len(v)) :
+            R[k][j] = dotProduct(conjTransVec(Q[j]), v[k])
+            v[k] = vectorSub(v[k], scalarVecMulti(R[k][j], Q[j]))
+
+    return Q, R
+
+def backSub(A, b) :
+    """Uses back substitution to solve a matrix system.
+
+    This function takes in a multidimensional list, A, representing an upper
+    triangular matrix and a vector representing the solutions for the system,
+    and it computes the variables of the system.
+
+    Args:
+        A (list): Multidimensional list representing a matrix.
+        b (list): List representing a vector.
+
+    Returns:
+        (list): The variables of the system.
+
+    """
+
+    result = [b[i] for i in range(len(b))]
+    for i in range(len(A[0]) - 1, -1, -1) :
+        result[i] = (b[i] - _sum([A[k][i] * result[k] for k in range(i + 1, len(A[0]))])) / A[i][i]
+    return result
+
+def interpolate(a, x) :
+    """Calculates the interpolation of a polynomial.
+
+    This function takes in a list of coefficients and calculates
+    the solution to a polynomial of degree len(a) with coefficients
+    a at x.
+
+    Args:
+        a (list): This is a list of numbers to be used a coefficients
+        x (int/float/complex): This is a number to be plugged into the polynomial.
+
+    """
+
+    return _sum([a[i] * (x ** i) for i in range(len(a))])
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from computation import Vector
-from computation import Matrix
+from computation import vandermonde
+from computation import modGramSchmidt
+from computation import matVecMulti
+from computation import conjTransMat
+from computation import backSub
+from computation import interpolate
 
-a = Matrix([
-    [1,2],
-    [3,4]
-])
-b = Vector([1,2])
+def getCoefficients(x, y) :
+    A = vandermonde(x, 3)
+    Q,R = modGramSchmidt(A)
+    b = matVecMulti(conjTransMat(Q), y)
+    a = backSub(R, b)
+    return a
 
-print(a * b)
+a = getCoefficients([0,1,2,3,4], [0,1,4,9,16])
+print(interpolate(a, 5))
